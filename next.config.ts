03bb961c4557096455055a62b7995339293096d8
0xVoushi <wallsof11@gmail.com>
2026-04-12 21:17:55 +0300

    feat: redirect /tutorial -> /guide (301 permanent)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/tutorial',
+        destination: '/guide',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
